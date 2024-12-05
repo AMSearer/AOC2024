b9152f14f6dcc95d77288980137489d24c698cfb
amsearer <a.m.searer@gmail.com>
2024-12-05 14:53:57 -0500

    Corrected input ingestion

--- AOC5.py
+++ AOC5.py
@@ -6,15 +6,24 @@ fileName = os.path.basename(__file__);
 
 inputPath = os.path.dirname(__file__) + "/Inputs/" + fileName[0: len(fileName)-3] + "_in.txt";
 
-input = []
-
-count = 0
-
 with open(inputPath, 'r') as file:
     lines = file.read()
 
     input = lines.split("\n\n")
 
 rules = input[0].split("\n")
-updates = input[1].split("\n")
+updatesStr = (input[1].split("\n"))
+updates = []
 
+updates.append([[int(y) for y in x.split(',')] for x in updatesStr])
+
+# updates.append(x for x in updatesStr)
+
+for updateStr in updatesStr:
+    # updates.append(int(x) for x in updateStr.split(','))
+    # print(updateStr.split(','))
+    intList = [int(num) for num in updateStr.split(',')]
+    # print(intList)
+
+
+print(updates)
